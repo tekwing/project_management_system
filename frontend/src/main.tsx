1ed4a4adc68167bc5d27646f9b416a386a778abf
tekwing <ajith969818@gmail.com>
2026-09-16 17:15:33 +0530

    Update main.tsx for production build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
       <App />
     </BrowserRouter>
   </StrictMode>,
