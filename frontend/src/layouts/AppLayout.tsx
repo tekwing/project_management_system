9152715f91cb0012dc7225d5bd6cff5a312e05bb
tekwing <ajith969818@gmail.com>
2026-09-16 17:23:06 +0530

    Add scroll-to-top support for step forms in AppLayout

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,10 +1,12 @@
 import Header from './../components/layout/Header';
 import Sidebar from './../components/layout/Sidebar';
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function AppLayout(){
     const [collapsed, setCollapsed] = useState(false);
+    const parentDivRef = useRef<HTMLElement | null>(null);
+
     console.log("collapsed:", collapsed);
     return(
         <div className="h-screen flex overflow-hidden">
@@ -13,8 +15,8 @@ export default function AppLayout(){
 
                     <Header collapsed={collapsed} onToggle={() => setCollapsed(!collapsed)}/>
 
-                    <main className="flex-1 p-6 bg-gray-100 overflow-y-auto">
-                        <Outlet />
+                    <main ref={parentDivRef} className="flex-1 p-6 bg-gray-100 overflow-y-auto">
+                        <Outlet context={{ parentDivRef }} />
                     </main>
 
                 </div>   
